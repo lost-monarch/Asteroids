e3f05e2754d9a1f63c89ebcb2b817aad224edb60
lost-monarch <oac.bwe@gmail.com>
2025-10-23 19:58:46 -0400

    Added Player Movement

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ def main():
     player = Player(x, y)
 
     a = 1
-    while a > 0:
+    while True:
+        dt = clock.tick(60)/1000
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
 
-    dt = clock.tick(60)/1000
+
+
 
 
 if __name__ == "__main__":
