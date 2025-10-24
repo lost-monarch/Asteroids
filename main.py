9ea27ef6e2b0a263e4f5d64f2d6a09994523e1b3
lost-monarch <oac.bwe@gmail.com>
2025-10-23 20:18:27 -0400

    Created groups for classes

--- main.py
+++ main.py
@@ -9,22 +9,28 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
 
-    a = 1
     while True:
-        dt = clock.tick(60)/1000
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 
 
